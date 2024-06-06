4e03c736668e39d2f219017c67fc6619723e05b0
abt93 <anthony.temmel@gmail.com>
2024-06-06 12:30:57 +0200

    Ajout des fonctions Substract, Mutiply, Divide et Modulo

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 class MainClass
 {
@@ -24,5 +25,25 @@ class MainClass
     public static double Add(double x, double y)
     {
         return x + y;
+    }
+
+    public static double Substract(double x, double y)
+    {
+        return x - y;
+    }
+
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public static double Divide(double x, double y)
+    {
+        return x / y;
+    }
+
+    public static double Modulo(int x, int y)
+    {
+        return x % y;
     }
 }
