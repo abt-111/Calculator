06ee7b3538b1ad8e7c1bf617f4b5c131e6764b90
abt93 <anthony.temmel@gmail.com>
2024-06-06 12:22:11 +0200

    Ajout de la saisie de l'utilisateur et de la fonction Add

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        // Code
+        double firstOperand = 0;
+        double secondOperand = 0;
+        string operation = "";
+
+        Console.WriteLine("first operand: ");
+        firstOperand = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("second operand: ");
+        secondOperand = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("which operation you choose: ");
+        operation = Console.ReadLine();
+
+        Console.WriteLine(firstOperand + " + " + secondOperand + " = " + Add(firstOperand, secondOperand));
+    }
+
+    public static double Add(double x, double y)
+    {
+        return x + y;
     }
 }
