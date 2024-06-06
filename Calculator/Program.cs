1dac2cdc1fb285fd5a4ff90981789e5e073f4d5c
abt93 <anthony.temmel@gmail.com>
2024-06-06 14:22:27 +0200

    Ajout sélection du type d'opération

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -16,34 +16,67 @@ class MainClass
         Console.WriteLine("second operand: ");
         secondOperand = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("which operation you choose: ");
+        Console.WriteLine("which operation you choose (add, sub, multi, div, mod): ");
         operation = Console.ReadLine();
 
-        Console.WriteLine(firstOperand + " + " + secondOperand + " = " + Add(firstOperand, secondOperand));
+        switch(operation)
+        {
+            case "add":
+                Add(firstOperand, secondOperand);
+                break;
+            case "sub":
+                Substract(firstOperand, secondOperand);
+                break;
+            case "multi":
+                Multiply(firstOperand, secondOperand);
+                break;
+            case "div":
+                Divide(firstOperand, secondOperand);
+                break;
+            case "mod":
+                Modulo((int)firstOperand, (int)secondOperand);
+                break;
+            default:
+                break;
+        }
     }
 
-    public static double Add(double x, double y)
+    public static void Add(double x, double y)
     {
-        return x + y;
+        Console.WriteLine(x + " + " + y + " = " + (x + y));
     }
 
-    public static double Substract(double x, double y)
+    public static void Substract(double x, double y)
     {
-        return x - y;
+        Console.WriteLine(x + " - " + y + " = " + (x - y));
     }
 
-    public static double Multiply(double x, double y)
+    public static void Multiply(double x, double y)
     {
-        return x * y;
+        Console.WriteLine(x + " * " + y + " = " + (x * y));
     }
 
-    public static double Divide(double x, double y)
+    public static void Divide(double x, double y)
     {
-        return x / y;
+        if(y == 0)
+        {
+            Console.WriteLine("Pas diviseur égale 0 petit malin");
+        }
+        else
+        {
+            Console.WriteLine(x + " / " + y + " = " + (x / y));
+        }
     }
 
-    public static double Modulo(int x, int y)
+    public static void Modulo(int x, int y)
     {
-        return x % y;
+        if (y == 0)
+        {
+            Console.WriteLine("Pas diviseur égale 0 petit malin");
+        }
+        else
+        {
+            Console.WriteLine(x + " % " + y + " = " + (x % y));
+        }
     }
 }
